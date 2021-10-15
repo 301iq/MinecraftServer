dc10c0705a043fb45a60d99074d1eb35107a172f
AlexFire-Dev <74737162+AlexFire-Dev@users.noreply.github.com>
2021-10-15 17:08:30 +0300

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-wget -O server.jar -P server https://download.getbukkit.org/spigot/spigot-1.17.1.jar
+wget -O server.jar https://download.getbukkit.org/spigot/spigot-1.17.1.jar
+mv server.jar server
